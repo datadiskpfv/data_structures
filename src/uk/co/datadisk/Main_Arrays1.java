3638a2754e67823d4387e9e0e8ffdc0171c6d068
pvalle <paul.valle@orbian.com>
2018-08-13 13:40:56 +0100

    looking at big-o notation of arrays

--- src/uk/co/datadisk/Main_Arrays1.java
+++ src/uk/co/datadisk/Main_Arrays1.java
@@ -3,7 +3,17 @@ package uk.co.datadisk;
 public class Main_Arrays1 {
 
     public static void main(String[] args) {
+
+        // O(1) = retrieve with index
+        // O(n) = retrieve without index
+        // O(n) = add an element to full array
+        // O(1) = add an element to end of an array
+        // O(1) = insert or update an element at specific index
+        // O(1) = delete an element by setting it to null
+        // O(n) = delete an element by shifting elements
+
         // Arrays are not dynamic, size is static
+        // every element in array is the same size depending on data type
         int[] myArray1 = new int[7];
 
         // arrays are zero base, so we have 0-6 indexes
@@ -19,6 +29,14 @@ public class Main_Arrays1 {
 
         for( int i = 0; i < myArray1.length; i++){
             System.out.println(myArray1[i]);
+        }
+
+        for( int i = 0; i < myArray1.length; i++){
+            System.out.print(".");
+            if(myArray1[i] == 7){
+                System.out.println("\nWe have found 7 at index " + i);
+                break;
+            }
         }
     }
 }
