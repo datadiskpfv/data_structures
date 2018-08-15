a9f9cfff96bd864070cc4ed88280ba9305bad80d
pvalle <paul.valle@orbian.com>
2018-08-15 14:14:50 +0100

    looked at quick sort

--- src/uk/co/datadisk/Main_Merge_Sort.java
+++ src/uk/co/datadisk/Main_Merge_Sort.java
@@ -7,9 +7,7 @@ public class Main_Merge_Sort {
 
         mergeSort(bubbleArray, 0, bubbleArray.length);
 
-        for(int i = 0; i < bubbleArray.length; i++){
-            System.out.println(bubbleArray[i]);
-        }
+        displayArray(bubbleArray, "bubbleArray");
     }
 
     public static void mergeSort(int[] input, int start, int end){
@@ -20,15 +18,20 @@ public class Main_Merge_Sort {
 
         // get the middle of the array
         int mid = (start + end) / 2;
+        System.out.println("MergeSort mid: " + mid);
         // left side of array
+        System.out.println("mergeSort calling left side start: " + start +  " mid: " + mid);
         mergeSort(input, start, mid);
         // right side of the array
+        System.out.println("mergeSort calling right side mid: " + mid + " end: " + end);
         mergeSort(input, mid, end);
 
         merge(input, start, mid, end);
     }
 
     public static void merge(int[] input, int start, int mid, int end){
+
+        System.out.println("merge called........ start: " + start + " mid: " + mid + " end: " + end);
 
         if(input[mid - 1] <= input[mid]){
             return;
@@ -38,15 +41,33 @@ public class Main_Merge_Sort {
         int j = mid;
         int tempIndex = 0;
 
+        System.out.println("merge i: " + i + " j: " + j + " tempIndex: " + tempIndex);
+
         int[] temp = new int[end - start];
         while(i < mid && j < end){
             // <= makes it a stable array to handle duplicates
             temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+            displayArray(temp, "temp");
+            System.out.println();
         }
 
         // src, srcPosition, dest, destPosition, length
+        System.out.println("merge - input copy i: " + i + " destPos: " + (start + tempIndex) + " length: " + (mid - 1));
         System.arraycopy(input, i, input, start + tempIndex, mid - i);
-        System.arraycopy(temp, 0, input, start, tempIndex);
+        displayArray(input, "input");
+        System.out.println();
+
+        System.out.println("merge - temp copy start: " + start + " tempIndex: " + tempIndex);
+        System.arraycopy(temp, 0, input, start,  tempIndex);
+        displayArray(input, "input");
+        System.out.println();
+    }
+
+    public static void displayArray(int[] input, String name){
+        System.out.print("Array " + name + " - ");
+        for(int i = 0; i < input.length; i++){
+            System.out.print(input[i] + " ");
+        }
     }
 
 }
---
+++ src/uk/co/datadisk/Main_Quick_Sort.java
@@ -0,0 +1,65 @@
+package uk.co.datadisk;
+
+public class Main_Quick_Sort {
+
+    public static void main(String[] args) {
+        int[] bubbleArray = {20, 35, -15, 7, 55, 1, -22};
+
+        displayArray(bubbleArray, "bubbleArray - Start");
+
+        quickSort(bubbleArray, 0, bubbleArray.length);
+
+        displayArray(bubbleArray, "bubbleArray");
+
+    }
+
+    public static void quickSort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+        quickSort(input, pivotIndex + 1, end);
+
+    }
+
+    private static int partition(int[] input, int start, int end) {
+        // This is using the first element as the pivot
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+            // Empty loop body
+            // looking from right to left (smaller elements)
+            while (i < j && input[--j] >= pivot);
+            if (i < j) {
+                System.out.println("Replacing " + input[i] + " with " + input[j]);
+                input[i] = input[j];
+                displayArray(input, "input");
+            }
+
+            // looking from left to right (larger elements)
+            while (i < j && input[++i] <= pivot);
+            if (i < j) {
+                System.out.println("Replacing " + input[j] + " with " + input[i]);
+                input[j] = input[i];
+                displayArray(input, "input");
+            }
+        }
+
+        input[j] = pivot;
+        displayArray(input, "input");
+        System.out.println();
+        return j;
+    }
+
+    public static void displayArray(int[] input, String name) {
+        System.out.print("Array " + name + " - ");
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
+        System.out.println("\n");
+    }
+}
