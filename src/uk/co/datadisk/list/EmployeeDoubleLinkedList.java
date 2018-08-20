59f59bc441da37bd6223a422cc0505e885d155e5
Paul Valle <paul.valle@datadisk.co.uk>
2018-08-20 18:16:21 +0100

    looking at linked lists sinle and double

--- src/uk/co/datadisk/list/EmployeeDoubleLinkedList.java
+++ src/uk/co/datadisk/list/EmployeeDoubleLinkedList.java
@@ -8,12 +8,12 @@ public class EmployeeDoubleLinkedList {
 
   public void addToFront(Employee employee){
     EmployeeDoubleNode node = new EmployeeDoubleNode(employee);
-    node.setNext(head);
 
     if(head == null){
       tail = node;
     } else {
       head.setPrevious(node);
+      node.setNext(head);
     }
 
     head = node;
@@ -59,10 +59,34 @@ public class EmployeeDoubleLinkedList {
     }
 
     EmployeeDoubleNode removedNode = head;
-    head = head.getNext();
 
+    if(head.getNext() == null){
+      tail = null;
+    } else {
+      head.getNext().setPrevious(null);
+    }
+    head = head.getNext();
     size--;
     removedNode.setNext(null);
+    return removedNode;
+  }
+
+  public EmployeeDoubleNode removeFromEnd() {
+    if(isEmpty()){
+      return null;
+    }
+
+    EmployeeDoubleNode removedNode = tail;
+
+    if( tail.getPrevious() == null){
+      head = null;
+    } else {
+      tail.getPrevious().setNext(null);
+    }
+
+    tail = tail.getPrevious();
+    size--;
+    removedNode.setPrevious(null);
     return removedNode;
   }
 }
--- src/uk/co/datadisk/list/Main_Double_Linked_List.java
+++ src/uk/co/datadisk/list/Main_Double_Linked_List.java
@@ -9,8 +9,6 @@ public class Main_Double_Linked_List {
     Employee dominic = new Employee("Dominic", "Valle", 1002);
     Employee jessica = new Employee("Jessica", "Valle", 1003);
 
-    Employee will = new Employee("Will", "Hay", 1004);
-
     EmployeeDoubleLinkedList list = new EmployeeDoubleLinkedList();
 
     list.addToFront(paul);
@@ -19,7 +17,20 @@ public class Main_Double_Linked_List {
     list.addToFront(jessica);
 
     list.printList();
+    System.out.println("List size: " + list.getSize());
 
+    Employee will = new Employee("Will", "Hay", 1004);
+    list.addToEnd(will);
 
+    list.printList();
+    System.out.println("List size: " + list.getSize());
+
+    list.removeFromFront();
+    list.printList();
+    System.out.println("List size: " + list.getSize());
+
+    list.removeFromEnd();
+    list.printList();
+    System.out.println("List size: " + list.getSize());
   }
 }
