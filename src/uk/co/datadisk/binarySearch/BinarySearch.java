2c1b9ff0522270275ad74dad55c1beaf46992fcd
datadiskpfv <paul.valle@datadisk.co.uk>
2018-09-01 11:12:21 +0100

    looking at binary search

---
+++ src/uk/co/datadisk/binarySearch/BinarySearch.java
@@ -0,0 +1,11 @@
+package uk.co.datadisk.binarySearch;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        // ARRAY MUST BE SORTED !!!!!!
+        int[] intArray = { -22, -15, 1, 7, 20, 35, 55 };
+
+    }
+}
