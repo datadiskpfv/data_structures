e704bc2c74fe14dfb9c69bf54b4c7cacaba74506
datadiskpfv <paul.valle@datadisk.co.uk>
2018-09-01 17:03:18 +0100

    looking at heaps

---
+++ src/uk/co/datadisk/heap/Main_MaxHeap.java
@@ -0,0 +1,9 @@
+package uk.co.datadisk.heap;
+
+public class Main_MaxHeap {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/uk/co/datadisk/heap/MaxHeap.java
@@ -0,0 +1,95 @@
+package uk.co.datadisk.heap;
+
+public class MaxHeap {
+
+    private int[] heap;
+    private int size;
+
+    public MaxHeap(int capacity) {
+        heap = new int[capacity];
+    }
+
+    public void insert(int value) {
+        if (isFull()) {
+            throw new IndexOutOfBoundsException("Heap is full");
+        }
+
+        heap[size] = value;
+        fixHeapAbove(size);
+        size++;
+    }
+
+    public int delete(int index) {
+        if (isEmpty()){
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+
+
+    }
+
+    private void fixHeapAbove(int index) {
+        int newValue = heap[index];
+        while ( index > 0 && newValue > heap[getParent(index)] ) {
+           heap[index] = heap[getParent(index)];
+           index = getParent(index);
+        }
+
+        heap[index] = newValue;
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex){
+        int childToSwap;
+
+        while(index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+
+            if(leftChild <= lastHeapIndex){
+                if(rightChild > lastHeapIndex){
+                    childToSwap = leftChild;
+                } else {
+                    childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+                }
+
+                if (heap[index] < heap[childToSwap]) {
+                    int tmp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = tmp;
+                } else {
+                    break;
+                }
+
+                index = childToSwap;
+            } else {
+                break;
+            }
+        }
+    }
+
+
+
+
+
+    public boolean isFull() {
+        return size == heap.length;
+    }
+
+    public int getParent(int index) {
+        return (index - 1) / 2;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
+    }
+
+
+}
