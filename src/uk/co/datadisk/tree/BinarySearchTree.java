adabad6bf8e346723f7c0de35c6790fc81094534
datadiskpfv <paul.valle@datadisk.co.uk>
2018-09-01 15:31:21 +0100

    looking at trees and tree orders

---
+++ src/uk/co/datadisk/tree/BinarySearchTree.java
@@ -0,0 +1,30 @@
+package uk.co.datadisk.tree;
+
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(15);
+        intTree.insert(27);
+        intTree.insert(30);
+        intTree.insert(29);
+        intTree.insert(26);
+        intTree.insert(22);
+        intTree.insert(32);
+        intTree.insert(17);
+        System.out.println("Tree created");
+
+        intTree.traverseInOrder();
+
+        System.out.println(intTree.get(27));
+        System.out.println(intTree.get(17));
+        System.out.println(intTree.get(8888));
+
+        System.out.println();
+        System.out.println("MIN value = " + intTree.min());
+        System.out.println("MAX value = " + intTree.max());
+    }
+}
