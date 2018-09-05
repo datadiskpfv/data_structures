f8302e4e2a058055926a6cf60557a69f7de08469
pvalle <paul.valle@orbian.com>
2018-09-05 13:33:51 +0100

    looked at MultiMap from Guava, Map keys can have multiple values

--- src/uk/co/datadisk/scratch/map/Main_MultiMap_1.java
+++ src/uk/co/datadisk/scratch/map/Main_MultiMap_1.java
@@ -1,9 +1,11 @@
 package uk.co.datadisk.scratch.map;
 
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Multimap;
 
 import java.util.Collection;
+import java.util.List;
 
 public class Main_MultiMap_1 {
 
@@ -11,23 +13,33 @@ public class Main_MultiMap_1 {
 
     public static void main(String[] args) {
 
-        Multimap<String, String> myMultimap = ArrayListMultimap.create();
+        // There are many MultiMap types: BIMap, ListMultimap, SetMultimap and SortedSetMultimap.
+
+        // ListMultimap<String, String> multimap = ArrayListMultimap.create();
+        // Multiset<Class> myMultiset = HashMultiset.create();
+        // BiMap<String,String> myBiMap = HashBiMap.create();
+
+        // Multimap<String, String> myMultimap = ArrayListMultimap.create();
+        ListMultimap<String, String> myMultimap = ArrayListMultimap.create();
 
         // Adding some key/value
         myMultimap.put("Fruits", "Banana");
         myMultimap.put("Fruits", "Apple");
         myMultimap.put("Fruits", "Pear");
         myMultimap.put("Vegetables", "Carrot");
+        myMultimap.put("Vegetables", "Potatoes");
 
         // Getting the size
         int size = myMultimap.size();
         System.out.println(size);  // 4
 
         // Getting values
-        Collection<String> fruits = myMultimap.get("Fruits");
+        //Collection<String> fruits = myMultimap.get("Fruits");                 // used with Multimap
+        List<String> fruits = myMultimap.get("Fruits");
         System.out.println("Fruits: " + fruits); // [Banana, Apple, Pear]
 
-        Collection<String> vegetables = myMultimap.get("Vegetables");
+        // Collection<String> vegetables = myMultimap.get("Vegetables");        // used with Multimap
+        List<String> vegetables = myMultimap.get("Vegetables");
         System.out.println("Vegetables: " + vegetables); // [Carrot]
 
         // Iterating over entire Multimap
@@ -43,7 +55,6 @@ public class Main_MultiMap_1 {
 
         // Remove all values for a key
         myMultimap.removeAll("Fruits");
-        System.out.println("Fruoits are removing everything: " + myMultimap.get("Fruits")); // [] (Empty Collection!)
-
+        System.out.println("Fruits are removing everything: " + myMultimap.get("Fruits")); // [] (Empty Collection!)
     }
 }
